ed09ff47cbf5e1e6456aa538d1f06c580487f8f4
Lazybones3 <caorui452@163.com>
2024-04-20 16:11:30 +0800

    Tutorial2

---
+++ Tutorial2/mainwindow.cpp
@@ -0,0 +1,9 @@
+#include <KTextEdit>
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : KXmlGuiWindow(parent) {
+    textArea = new KTextEdit();
+    setCentralWidget(textArea);
+    setupGUI();
+}
